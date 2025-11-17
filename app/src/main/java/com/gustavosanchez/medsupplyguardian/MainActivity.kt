d1eceb30aacb38f93ea0a0bc561407ba54e5f217
SiDeRaL12 <000486822@student.vcc.ca>
2025-11-17 04:54:43 +0300

    Initial project setup: Empty Compose Activity with Material 3
    - Project name: MedSupplyGuardian
    - Package: com.gustavosanchez.medsupplyguardian
    - Min SDK: 24, Target SDK: 35
    - Dependencies: Room, Navigation Compose, Coroutines
    - Author: Gustavo Sanchez
    - Purpose: Healthcare supply compliance audit application

---
+++ app/src/main/java/com/gustavosanchez/medsupplyguardian/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.gustavosanchez.medsupplyguardian
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.gustavosanchez.medsupplyguardian.ui.theme.MedSupplyGuardianTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MedSupplyGuardianTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MedSupplyGuardianTheme {
+        Greeting("Android")
+    }
+}
